69a80461c82b6a3ccf740806a50bf4ca75450cdf
unmanbearpig <unmanbearpig@gmail.com>
2017-04-27 03:49:47 +0300

    Add solution for OCR

--- ocr-numbers/src/OCR.hs
+++ ocr-numbers/src/OCR.hs
@@ -1,4 +1,61 @@
 module OCR (convert) where
 
+import Data.List (find, intercalate)
+import Data.List.Split (chunksOf)
+
+newtype Chunk = Chunk [String] deriving (Eq)
+data Character = Character { value :: String, charChunk :: Chunk }
+
+chars = [ Character "0" ( Chunk [ " _ "
+                                , "| |"
+                                , "|_|"
+                                , "   " ])
+        , Character "1" ( Chunk [ "   "
+                                , "  |"
+                                , "  |"
+                                , "   " ])
+        , Character "2" ( Chunk [ " _ "
+                                , " _|"
+                                , "|_ "
+                                , "   " ])
+        , Character "3" ( Chunk [ " _ "
+                                , " _|"
+                                , " _|"
+                                , "   " ])
+        , Character "4" ( Chunk [ "   "
+                                , "|_|"
+                                , "  |"
+                                , "   " ])
+        , Character "5" ( Chunk [ " _ "
+                                , "|_ "
+                                , " _|"
+                                , "   " ])
+        , Character "6" ( Chunk [ " _ "
+                                , "|_ "
+                                , "|_|"
+                                , "   " ])
+        , Character "7" ( Chunk [ " _ "
+                                , "  |"
+                                , "  |"
+                                , "   " ])
+        , Character "8" ( Chunk [ " _ "
+                                , "|_|"
+                                , "|_|"
+                                , "   " ])
+        , Character "9" ( Chunk [ " _ "
+                                , "|_|"
+                                , " _|"
+                                , "   " ]) ]
+
+
+matchChar :: [Character] -> Chunk -> Maybe Character
+matchChar cs chunk = find (\c -> chunk == charChunk c) cs
+
+splitCols :: Int -> [String] -> [Chunk]
+splitCols w ls
+  | lWidth < w = []
+  | otherwise = Chunk (take w <$> ls):splitCols w (drop w <$> ls)
+  where lWidth = length $ head ls
+
 convert :: String -> String
-convert xs = error "You need to implement this function."
+convert s = intercalate "," $ concat <$> fmap (maybe "?" value . matchChar chars) <$> splitCols 3 <$> chunksOf 4 (lines s)
